b30a2e1791084b7ea6e9f7508a59399ee058f62b
raaggarw <rajan.sien.aggarwal@cern.ch>
2019-05-22 20:39:14 +0200

    Add django-tables

--- website/detectors/views.py
+++ website/detectors/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django_tables2 import RequestConfig
+from .models import Detector
+from .tables import DetectorTable
+
+PER_PAGE_ROWS = 25
 
 # Create your views here.
 
 @login_required(login_url='login/')
 def home(request):
-	return render(request, 'home.html')
+	detectors_table = DetectorTable(Detector.objects.all())
+	RequestConfig(request, paginate={'per_page': PER_PAGE_ROWS}).configure(detectors_table)
+	return render(request, 'home.html', {'detectors_table': detectors_table})
 
 def logout_view(request):
 	logout(request)
